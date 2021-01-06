6b2e9eedc103dd5d578a61c12a5130dae513dcca
Felipe Lara <69820156+FelipeLaras@users.noreply.github.com>
2021-01-06 13:42:08 -0300

    Update pdf_termo.php

--- inc/pdf_termo.php
+++ inc/pdf_termo.php
@@ -254,6 +254,7 @@ require_once '../dompdf/lib/html5lib/Parser.php';
 require_once '../dompdf/lib/php-font-lib/src/FontLib/Autoloader.php';
 require_once '../dompdf/lib/php-svg-lib/src/autoload.php';
 require_once '../dompdf/src/Autoloader.php';
+
 Dompdf\Autoloader::register();
 
 // reference the Dompdf namespace
